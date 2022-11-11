f6abd5b8b6101d06eae7297e87fea65016d06836
Daniel Suescun <profesionalsistemas14@pharmasan.co>
2022-11-11 10:14:33 -0500

    :zap: Modificacion endpoint margen bruto

--- src/dashboard/infrastructure/repositories/getMargenBruto.repository.js
+++ src/dashboard/infrastructure/repositories/getMargenBruto.repository.js
@@ -3,6 +3,7 @@ const dayjs = require('dayjs')
 const _ = require('lodash')
 
 module.exports = async (anio) => {
+    const arrayResult = []
     const fechaInicio = Date.parse(dayjs(`${anio}-01-01`)) / 1000;
     const fechaFin = Date.parse(dayjs(`${anio}-12-31`)) / 1000;
     let result = await db.collection('Facturas')
@@ -14,11 +15,40 @@ module.exports = async (anio) => {
         ...doc.data()
     }))
     result = result.map(factura => {
-        factura.fecha = dayjs(factura.fecha._seconds * 1000).format('DD/MM/YYYY')
+        const mes = dayjs(factura.fecha._seconds * 1000).month() + 1
+        factura.fecha = dayjs(factura.fecha._seconds * 1000).format('DD-MM-YYYY')
         return {
-            ...factura
+            ...factura,
+            mes
         }
     })
 
-    return _.groupBy(result, factura => factura.tipo);
+    result = _.groupBy(result, factura => factura.mes);
+
+    for (const [index, item] of Object.entries(result)) {
+        let compras = 0
+        let ventas = 0
+        item.map(factura => {
+            if (factura.tipo === 1) {
+                ventas += factura.precio * factura.cantidad
+            } else if (factura.tipo === 2) {
+                compras += factura.precio * factura.cantidad
+            }
+        })
+        result[index] = ventas ? Math.trunc(((ventas - compras) / ventas) * 100) : 0
+        // result[index] = {
+        //     ventas,
+        //     compras,
+        // }
+    }
+
+    for (let i = 1; i <= 12; i++) {
+        if (typeof result[i] === 'undefined') {
+            arrayResult.push(0)
+        } else {
+            arrayResult.push(result[i] > 0 ? result[i] : 0)
+        }
+    }
+
+    return arrayResult
 }
